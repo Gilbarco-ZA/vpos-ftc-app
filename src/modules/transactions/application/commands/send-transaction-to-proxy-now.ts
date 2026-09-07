7bc25a5bae3bad12d812b662bd22721757dc3ebc
Ben Henning <benhenning10@gmail.com>
2026-09-07 11:39:09 +0200

    Block manual send until pre-fuel customer is attached

--- src/modules/transactions/application/commands/send-transaction-to-proxy-now.ts
+++ src/modules/transactions/application/commands/send-transaction-to-proxy-now.ts
@@ -4,10 +4,28 @@ import {
   requiresPreFiscalizationReceiptPrint,
 } from '../../infrastructure/fiscalization/autoPrintFiscalReceipt'
 import { sendTransactionToProxyNow } from '../../infrastructure/fiscalization/proxySenderWorker'
+import { getTinCaptureOrderRepo } from '../../infrastructure/preFuelCustomerAllocation'
+import { getTransactionDetails } from '../queries/get-transaction-details'
 
 export async function sendTransactionToProxyNowCommand(
   input: Parameters<typeof sendTransactionToProxyNow>[0],
 ) {
+  if ((await getTinCaptureOrderRepo(input.stationId)) === 'before_transaction') {
+    const transaction = await getTransactionDetails(
+      input.stationId,
+      input.transactionId,
+    )
+    const customerId = transaction?.customer_id ?? transaction?.customerId ?? null
+    if (!customerId) {
+      return {
+        ok: false as const,
+        skipped: true as const,
+        reason:
+          'Pre-transaction customer allocation has not yet been attached to this sale.',
+      }
+    }
+  }
+
   if (await requiresPreFiscalizationReceiptPrint(input.stationId)) {
     const print = await enqueueAutoPrintFiscalReceipt({
       stationId: input.stationId,
